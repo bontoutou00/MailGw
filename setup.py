334cc622daf2d32a7e0a4ad059ef3ce9de92430f
bontoutou00 <bontoutou0+test@protonmail.com>
2022-06-22 12:58:52 +0200

    FIx name

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="MailGw",
-    version="0.0.2",
+    name="MailGw Temporary Email",
+    version="0.0.1",
     author="bontoutou",
     description="10 Minute Temporary Email",
     long_description=long_description,
